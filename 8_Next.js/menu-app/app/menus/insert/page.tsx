8f6ceb3f01b00515189b9c53abcf6c8461e83f1a
DESKTOP-9UP2K8D\user1 <duddn1564@gmail.com>
2026-04-16 17:31:50 +0900

    0416 Next.js 커밋

---
+++ 8_Next.js/menu-app/app/menus/insert/page.tsx
@@ -0,0 +1,11 @@
+import MenuInsertFrom from "@/app/components/menus/MenuInsertForm";
+
+
+export default function MenuInsertPage(){
+
+    return(
+        <div className="container">
+            <MenuInsertFrom/>
+        </div>
+    )
+}
